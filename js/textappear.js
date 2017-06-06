9e5b4c4bbf19cfc51b6d18d3356d7f81e26e4d0f
Matt Linenberger <mattlinenberger@gmail.com>
2017-06-06 02:14:33 -0500

    added .js function to add and remove text

---
+++ js/textappear.js
@@ -0,0 +1,48 @@
+function appear(name){
+    
+  
+           document.getElementById("text").innerHTML = name;
+   
+        console.log(name);
+
+
+}
+
+function appear2(name2){
+   
+       
+             document.getElementById("text2").innerHTML = name2;
+       
+       
+        console.log(name2);
+
+
+}
+function appear3(name3){
+   
+       
+             document.getElementById("text3").innerHTML = name3;
+       
+       
+        console.log(name3);
+
+
+}
+function appear4(name4){
+   
+       
+             document.getElementById("text4").innerHTML = name4;
+       
+       
+        console.log(name4);
+
+
+}
+
+function disappear(blank){
+     document.getElementById("text4").innerHTML = blank;
+        document.getElementById("text3").innerHTML = blank;
+        document.getElementById("text2").innerHTML = blank;
+        document.getElementById("text").innerHTML = blank;
+       
+}
