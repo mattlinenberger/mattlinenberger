72bfc0e28d2598811a8f3425e5665772b766d92c
Matt Linenberger <mattlinenberger@gmail.com>
2017-06-08 18:56:17 -0500

    Added second project modal and minor css changes

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,11 @@
+//Start of virst modal images (bobs burgers)
 // Get the modal
 var modal = document.getElementById('myModal');
-
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
-
-
 var modalImg = document.getElementById("img01");
-
 var captionText = document.getElementById("caption");
+
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -41,6 +39,83 @@ var img4 = document.getElementById('myImg4');
 img4.onclick = function(){
     modal4.style.display = "block";
     modalImg4.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+// Get the modal
+var modal05 = document.getElementById('myModal05');
+var modalImg05 = document.getElementById("img005");
+var img05 = document.getElementById('myImg05');
+img05.onclick = function(){
+    modal05.style.display = "block";
+    modalImg05.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+// Get the modal
+var modal06 = document.getElementById('myModal06');
+var modalImg06 = document.getElementById("img006");
+var img06 = document.getElementById('myImg06');
+img06.onclick = function(){
+    modal06.style.display = "block";
+    modalImg06.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+
+
+//End of First Modal Images
+//Start of Second Modal Images(Magazine DC comics)
+var modal5 = document.getElementById('myModal5');
+var modalImg5 = document.getElementById("img05");
+var img5 = document.getElementById('myImg5');
+img5.onclick = function(){
+    modal5.style.display = "block";
+    modalImg5.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+
+// Get the modal
+var modal6 = document.getElementById('myModal6');
+var modalImg6 = document.getElementById("img06");
+var img6 = document.getElementById('myImg6');
+img6.onclick = function(){
+    modal6.style.display = "block";
+    modalImg6.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+
+// Get the modal
+var modal7 = document.getElementById('myModal7');
+var modalImg7 = document.getElementById("img07");
+var img7 = document.getElementById('myImg7');
+img7.onclick = function(){
+    modal7.style.display = "block";
+    modalImg7.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+// Get the modal
+var modal8 = document.getElementById('myModal8');
+var modalImg8 = document.getElementById("img08");
+var img8 = document.getElementById('myImg8');
+img8.onclick = function(){
+    modal8.style.display = "block";
+    modalImg8.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+// Get the modal
+var modal9 = document.getElementById('myModal9');
+var modalImg9 = document.getElementById("img09");
+var img9 = document.getElementById('myImg9');
+img9.onclick = function(){
+    modal9.style.display = "block";
+    modalImg9.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+// Get the modal
+var modal10 = document.getElementById('myModal10');
+var modalImg10 = document.getElementById("img10");
+var img10 = document.getElementById('myImg10');
+img10.onclick = function(){
+    modal10.style.display = "block";
+    modalImg10.src = this.src;
     captionText.innerHTML = this.alt;
 };
 
