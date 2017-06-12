eb6b28741d97a3dcf2ca0f12c3b4c0d92b78830c
Matt Linenberger <mattlinenberger@gmail.com>
2017-06-12 18:11:31 -0500

    commented out modal.js, not needed

--- js/modal.js
+++ js/modal.js
@@ -1,165 +1,165 @@
-// //Start of virst modal images (bobs burgers)
-// // Get the modal
-// var modal = document.getElementById('myModal');
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById('myImg');
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
+// // //Start of virst modal images (bobs burgers)
+// // // Get the modal
+// // var modal = document.getElementById('myModal');
+// // // Get the image and insert it inside the modal - use its "alt" text as a caption
+// // var img = document.getElementById('myImg');
+// // var modalImg = document.getElementById("img01");
+// // var captionText = document.getElementById("caption");
 
-// img.onclick = function(){
-//     modal.style.display = "block";
-//     modalImg.src = this.src;
-//     captionText.innerHTML = this.alt;
+// // img.onclick = function(){
+// //     modal.style.display = "block";
+// //     modalImg.src = this.src;
+// //     captionText.innerHTML = this.alt;
+// // };
+
+// // Get the modal
+// var modal2 = document.getElementById('myModal2');
+// var modalImg2 = document.getElementById("img02");
+// var img2 = document.getElementById('myImg2');
+// var captionText2 = document.getElementById("caption2");
+// img2.onclick = function(){
+//     modal2.style.display = "block";
+//     modalImg2.src = this.src;
+//     captionText2.innerHTML = this.alt;
 // };
 
-// Get the modal
-var modal2 = document.getElementById('myModal2');
-var modalImg2 = document.getElementById("img02");
-var img2 = document.getElementById('myImg2');
-var captionText2 = document.getElementById("caption2");
-img2.onclick = function(){
-    modal2.style.display = "block";
-    modalImg2.src = this.src;
-    captionText2.innerHTML = this.alt;
-};
+// // Get the modal
+// var modal3 = document.getElementById('myModal3');
+// var modalImg3 = document.getElementById("img03");
+// var img3 = document.getElementById('myImg3');
+// var captionText3 = document.getElementById("caption3");
+// img3.onclick = function(){
+//     modal3.style.display = "block";
+//     modalImg3.src = this.src;
+//     captionText3.innerHTML = this.alt;
+// };
 
-// Get the modal
-var modal3 = document.getElementById('myModal3');
-var modalImg3 = document.getElementById("img03");
-var img3 = document.getElementById('myImg3');
-var captionText3 = document.getElementById("caption3");
-img3.onclick = function(){
-    modal3.style.display = "block";
-    modalImg3.src = this.src;
-    captionText3.innerHTML = this.alt;
-};
-
-// Get the modal
-var modal4 = document.getElementById('myModal4');
-var modalImg4 = document.getElementById("img04");
-var img4 = document.getElementById('myImg4');
-var captionText4 = document.getElementById("caption4");
-img4.onclick = function(){
-    modal4.style.display = "block";
-    modalImg4.src = this.src;
-    captionText4.innerHTML = this.alt;
-};
-// Get the modal
-var modal05 = document.getElementById('myModal05');
-var modalImg05 = document.getElementById("img005");
-var img05 = document.getElementById('myImg05');
-var captionText5 = document.getElementById("caption5");
-img05.onclick = function(){
-    modal05.style.display = "block";
-    modalImg05.src = this.src;
-    captionText5.innerHTML = this.alt;
-};
-// Get the modal
-var modal06 = document.getElementById('myModal06');
-var modalImg06 = document.getElementById("img006");
-var img06 = document.getElementById('myImg06');
-var captionText6 = document.getElementById("caption6");
-img06.onclick = function(){
-    modal06.style.display = "block";
-    modalImg06.src = this.src;
-    captionText6.innerHTML = this.alt;
-};
+// // Get the modal
+// var modal4 = document.getElementById('myModal4');
+// var modalImg4 = document.getElementById("img04");
+// var img4 = document.getElementById('myImg4');
+// var captionText4 = document.getElementById("caption4");
+// img4.onclick = function(){
+//     modal4.style.display = "block";
+//     modalImg4.src = this.src;
+//     captionText4.innerHTML = this.alt;
+// };
+// // Get the modal
+// var modal05 = document.getElementById('myModal05');
+// var modalImg05 = document.getElementById("img005");
+// var img05 = document.getElementById('myImg05');
+// var captionText5 = document.getElementById("caption5");
+// img05.onclick = function(){
+//     modal05.style.display = "block";
+//     modalImg05.src = this.src;
+//     captionText5.innerHTML = this.alt;
+// };
+// // Get the modal
+// var modal06 = document.getElementById('myModal06');
+// var modalImg06 = document.getElementById("img006");
+// var img06 = document.getElementById('myImg06');
+// var captionText6 = document.getElementById("caption6");
+// img06.onclick = function(){
+//     modal06.style.display = "block";
+//     modalImg06.src = this.src;
+//     captionText6.innerHTML = this.alt;
+// };
 
 
-//End of First Modal Images
-//Start of Second Modal Images(Magazine DC comics)
-var modal5 = document.getElementById('myModal5');
-var modalImg5 = document.getElementById("img05");
-var img5 = document.getElementById('myImg5');
-var captionText7 = document.getElementById("caption7");
-img5.onclick = function(){
-    modal5.style.display = "block";
-    modalImg5.src = this.src;
-    captionText7.innerHTML = this.alt;
-};
+// //End of First Modal Images
+// //Start of Second Modal Images(Magazine DC comics)
+// var modal5 = document.getElementById('myModal5');
+// var modalImg5 = document.getElementById("img05");
+// var img5 = document.getElementById('myImg5');
+// var captionText7 = document.getElementById("caption7");
+// img5.onclick = function(){
+//     modal5.style.display = "block";
+//     modalImg5.src = this.src;
+//     captionText7.innerHTML = this.alt;
+// };
 
-// Get the modal
-var modal6 = document.getElementById('myModal6');
-var modalImg6 = document.getElementById("img06");
-var img6 = document.getElementById('myImg6');
-var captionText8 = document.getElementById("caption8");
-img6.onclick = function(){
-    modal6.style.display = "block";
-    modalImg6.src = this.src;
-    captionText8.innerHTML = this.alt;
-};
+// // Get the modal
+// var modal6 = document.getElementById('myModal6');
+// var modalImg6 = document.getElementById("img06");
+// var img6 = document.getElementById('myImg6');
+// var captionText8 = document.getElementById("caption8");
+// img6.onclick = function(){
+//     modal6.style.display = "block";
+//     modalImg6.src = this.src;
+//     captionText8.innerHTML = this.alt;
+// };
 
-// Get the modal
-var modal7 = document.getElementById('myModal7');
-var modalImg7 = document.getElementById("img07");
-var img7 = document.getElementById('myImg7');
-var captionText9 = document.getElementById("caption9");
-img7.onclick = function(){
-    modal7.style.display = "block";
-    modalImg7.src = this.src;
-    captionText9.innerHTML = this.alt;
-};
-// Get the modal
-var modal8 = document.getElementById('myModal8');
-var modalImg8 = document.getElementById("img08");
-var img8 = document.getElementById('myImg8');
-var captionText10 = document.getElementById("caption10");
-img8.onclick = function(){
-    modal8.style.display = "block";
-    modalImg8.src = this.src;
-    captionText10.innerHTML = this.alt;
-};
-// Get the modal
-var modal9 = document.getElementById('myModal9');
-var modalImg9 = document.getElementById("img09");
-var img9 = document.getElementById('myImg9');
-var captionText11 = document.getElementById("caption11");
-img9.onclick = function(){
-    modal9.style.display = "block";
-    modalImg9.src = this.src;
-    captionText11.innerHTML = this.alt;
-};
-// Get the modal
-var modal10 = document.getElementById('myModal10');
-var modalImg10 = document.getElementById("img10");
-var img10 = document.getElementById('myImg10');
-var captionText12 = document.getElementById("caption12");
-img10.onclick = function(){
-    modal10.style.display = "block";
-    modalImg10.src = this.src;
-    captionText12.innerHTML = this.alt;
-};
-//Start of Third Modal Section
-// Get the modal
-var modal11 = document.getElementById('myModal11');
-var modalImg11 = document.getElementById("img11");
-var img11 = document.getElementById('myImg11');
-var captionText13 = document.getElementById("caption13");
-img11.onclick = function(){
-    modal11.style.display = "block";
-    modalImg11.src = this.src;
-    captionText13.innerHTML = this.alt;
-};
-// Get the modal
-var modal12 = document.getElementById('myModal12');
-var modalImg12 = document.getElementById("img12");
-var img12 = document.getElementById('myImg12');
-var captionText14 = document.getElementById("caption14");
-img12.onclick = function(){
-    modal12.style.display = "block";
-    modalImg12.src = this.src;
-    captionText14.innerHTML = this.alt;
-};
-// Get the modal
-var modal13 = document.getElementById('myModal13');
-var modalImg13 = document.getElementById("img13");
-var img13 = document.getElementById('myImg13');
-var captionText15 = document.getElementById("caption15");
-img13.onclick = function(){
-    modal13.style.display = "block";
-    modalImg13.src = this.src;
-    captionText15.innerHTML = this.alt;
-};
+// // Get the modal
+// var modal7 = document.getElementById('myModal7');
+// var modalImg7 = document.getElementById("img07");
+// var img7 = document.getElementById('myImg7');
+// var captionText9 = document.getElementById("caption9");
+// img7.onclick = function(){
+//     modal7.style.display = "block";
+//     modalImg7.src = this.src;
+//     captionText9.innerHTML = this.alt;
+// };
+// // Get the modal
+// var modal8 = document.getElementById('myModal8');
+// var modalImg8 = document.getElementById("img08");
+// var img8 = document.getElementById('myImg8');
+// var captionText10 = document.getElementById("caption10");
+// img8.onclick = function(){
+//     modal8.style.display = "block";
+//     modalImg8.src = this.src;
+//     captionText10.innerHTML = this.alt;
+// };
+// // Get the modal
+// var modal9 = document.getElementById('myModal9');
+// var modalImg9 = document.getElementById("img09");
+// var img9 = document.getElementById('myImg9');
+// var captionText11 = document.getElementById("caption11");
+// img9.onclick = function(){
+//     modal9.style.display = "block";
+//     modalImg9.src = this.src;
+//     captionText11.innerHTML = this.alt;
+// };
+// // Get the modal
+// var modal10 = document.getElementById('myModal10');
+// var modalImg10 = document.getElementById("img10");
+// var img10 = document.getElementById('myImg10');
+// var captionText12 = document.getElementById("caption12");
+// img10.onclick = function(){
+//     modal10.style.display = "block";
+//     modalImg10.src = this.src;
+//     captionText12.innerHTML = this.alt;
+// };
+// //Start of Third Modal Section
+// // Get the modal
+// var modal11 = document.getElementById('myModal11');
+// var modalImg11 = document.getElementById("img11");
+// var img11 = document.getElementById('myImg11');
+// var captionText13 = document.getElementById("caption13");
+// img11.onclick = function(){
+//     modal11.style.display = "block";
+//     modalImg11.src = this.src;
+//     captionText13.innerHTML = this.alt;
+// };
+// // Get the modal
+// var modal12 = document.getElementById('myModal12');
+// var modalImg12 = document.getElementById("img12");
+// var img12 = document.getElementById('myImg12');
+// var captionText14 = document.getElementById("caption14");
+// img12.onclick = function(){
+//     modal12.style.display = "block";
+//     modalImg12.src = this.src;
+//     captionText14.innerHTML = this.alt;
+// };
+// // Get the modal
+// var modal13 = document.getElementById('myModal13');
+// var modalImg13 = document.getElementById("img13");
+// var img13 = document.getElementById('myImg13');
+// var captionText15 = document.getElementById("caption15");
+// img13.onclick = function(){
+//     modal13.style.display = "block";
+//     modalImg13.src = this.src;
+//     captionText15.innerHTML = this.alt;
+// };
 
 
 
