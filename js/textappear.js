39b918bab9e4b4548d7a77b25e46b47d3f8edea6
Matt Linenberger <mattlinenberger@gmail.com>
2017-06-12 19:58:33 -0500

    removed disappear function

--- js/textappear.js
+++ js/textappear.js
@@ -10,20 +10,19 @@ function appear2(name2){
         console.log(name2);
 }
 function appear3(name3){
-
+        
         document.getElementById("text3").innerHTML = name3;
         console.log(name3);
 }
 function appear4(name4){
-
          document.getElementById("text4").innerHTML = name4;
         console.log(name4);
 }
 
-function disappear(blank){
-         document.getElementById("text4").innerHTML = blank;
-        document.getElementById("text3").innerHTML = blank;
-        document.getElementById("text2").innerHTML = blank;
-        document.getElementById("text").innerHTML = blank;
+// function disappear(blank){
+//      document.getElementById("text4").innerHTML = blank;
+//         document.getElementById("text3").innerHTML = blank;
+//         document.getElementById("text2").innerHTML = blank;
+//         document.getElementById("text").innerHTML = blank;
        
-}
+// }
